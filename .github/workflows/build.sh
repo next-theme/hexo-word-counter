de95cda4871f4da443b410e81ed9c4ca8bfbbe44
Mimi <1119186082@qq.com>
2022-11-06 11:49:55 +0800

    Update

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,5 +1,6 @@
 ldd --version
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
 npm run build-release
-pip3 install boto3
+pip3 -m pip install boto3
 python3 deploy.py index.node hexo-word-counter/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
