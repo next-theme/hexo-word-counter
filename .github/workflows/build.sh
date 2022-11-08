db0e0f893d6bc652e3a92295c62ac2b960d41369
Mimi <1119186082@qq.com>
2022-11-08 11:20:38 +0800

    Run tests after build

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,6 +2,6 @@ ldd --version
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 npm run build-release
-node -e "console.log(require('./index.node'))"
+npm test
 python3 -m pip install boto3
 python3 .github/workflows/deploy.py index.node hexo-word-counter/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
