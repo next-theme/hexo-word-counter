5b7d54bee74858574cbd77bb21737fd4e896b72a
Mimi <1119186082@qq.com>
2024-04-16 12:58:43 +0800

    Remove actions-rs

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,5 +3,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 npm run build-release
 npm test
-python3 -m pip install boto3
-python3 .github/workflows/deploy.py index.node hexo-word-counter/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
+node .github/workflows/deploy.js index.node hexo-word-counter/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
