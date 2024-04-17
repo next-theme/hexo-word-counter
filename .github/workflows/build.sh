78b2e3710b153dd147ceefea56d6e1ba3b176b22
Mimi <1119186082@qq.com>
2024-04-17 23:58:03 +0800

    Add ubuntu-20.04

--- .github/workflows/build.sh
+++
@@ -1,6 +0,0 @@
-ldd --version
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source "$HOME/.cargo/env"
-npm run build-release
-npm test
-node .github/workflows/deploy.js index.node hexo-word-counter/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
