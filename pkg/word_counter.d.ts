792560c56b568df73830d4949b7acd4e8cda6dfa
Leon Qiu <i@leonvision.online>
2025-12-08 15:30:23 +0800

    build: untrack the pkg/

--- pkg/word_counter.d.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-
-export function wordCount(content: string): number;
--- pkg/word_counter_bg.wasm.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const wordCount: (a: number, b: number) => number;
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const __wbindgen_start: () => void;
